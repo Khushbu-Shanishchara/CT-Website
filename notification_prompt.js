5d4283bb653216d912ec1adddc26d5a2b1162068
Khushbu-Shanishchara <86378003+Khushbu-Shanishchara@users.noreply.github.com>
2024-04-25 11:38:18 +0530

    Update notification_prompt.js

--- notification_prompt.js
+++ notification_prompt.js
@@ -1,3 +1,4 @@
+/*
 clevertap.notifications.push({
   "titleText":'Would you like to receive Push Notifications?',
   "bodyText":'We promise to only send you relevant content and give you updates on your transactions',
@@ -6,3 +7,13 @@ clevertap.notifications.push({
   "okButtonColor":'#2e1825',
   "rejectButtonColor":'#987f62'
 });
+*/
+clevertap.notifications.push({
+   	"titleText": "Would you like to receive Push Notifications?",
+   	"bodyText": "We promise to only send you relevant content and give you updates on your transactions",
+   	"okButtonText": "Sign me up!",
+   	"rejectButtonText": "No thanks",
+   	"okButtonColor":"#F28046",
+   	"askAgainTimeInSeconds":20,
+  	"serviceWorkerPath": "/clevertap_sw.js" // path to your custom service worker file
+  });
